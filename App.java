59f49f959de2306bf759c5065da8ec36731a49d0
Tiramisu <tanakorn.tr@ku.th>
2025-07-10 17:09:47 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    /**
+     * Entry point for the program.
+     * Runs the manual test.
+     * @param args The command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        ShoppingCartManualTest.run();
+    }
+}
+
